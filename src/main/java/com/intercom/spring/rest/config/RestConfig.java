d00bd62933ab4813e512f206b68cb56086836089
Thilina Madumal <thilina.madumal@zalando.de>
2026-02-27 16:49:43 +0100

    open-api docs and gradle task to generate it

--- src/main/java/com/intercom/spring/rest/config/RestConfig.java
+++ src/main/java/com/intercom/spring/rest/config/RestConfig.java
@@ -3,14 +3,18 @@ package com.intercom.spring.rest.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zalando.problem.jackson.ProblemModule;
 import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @Configuration
-public class RestConfig {
+public class RestConfig implements WebMvcConfigurer {
 
   @Bean
   public ObjectMapper jacksonObjectMapper(final Jackson2ObjectMapperBuilder builder) {
@@ -20,6 +24,11 @@ public class RestConfig {
         .failOnUnknownProperties(false)
         .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
         .build();
+  }
+
+  @Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.add(0, new ByteArrayHttpMessageConverter());
   }
 }
 
